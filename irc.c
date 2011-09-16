53066eabb0f3056c10808fca8eda9ac434521546
Abdullah <abdullahibra@gmail.com>
2011-09-16 16:09:32 +0200

    irc client

---
+++ irc.c
@@ -0,0 +1,38 @@
+#include<u.h>
+#include<libc.h>
+
+#define TERM_NLINE "\r\n"
+
+void
+main(void)
+{
+	int fd;
+	char* buf[1024*1024];
+	int n;
+	
+	/*
+		connect to the server irc.freenode.net on the port 6667
+	*/
+	
+	fd = dial("tcp!irc.freenode.net!6667", nil, nil, nil);
+	if(fd < 0)
+		sysfatal("dial: %r");
+	
+	/*
+	 send your info to the server
+	*/	
+	
+	fprint(fd, "PASS asdf\r\n");
+	fprint(fd, "USER a b c :d\r\n");
+	fprint(fd, "NICK aibrahim\r\n");
+	fprint(fd, "PRIVMSG visof :Hello visof.\r\n");
+	
+	/*
+		get the info the server send to you
+	*/
+	
+	while(n = read(fd, buf, sizeof buf) > 0)
+		print("%s", buf);
+	 
+	exits(nil);	
+}
